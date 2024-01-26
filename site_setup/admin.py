f677dfbb4ec4a89c29dbcae785f3e1bd293156f4
Filipe <filipelisboaalves@gmail.com>
2024-01-26 12:35:34 -0300

    criando o model SiteSetup e registrando no admin

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 # Register your models here.
 
@@ -8,3 +8,12 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id','text','url_or_path', 
     list_display_links = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
+    
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display ='title','description', 
+    
+    #não permitir o usuario criar mais de um setup
+    def has_add_permission(self, request):
+        existe_uma_configuracao = bool(SiteSetup.objects.exists())
+        return not existe_uma_configuracao
